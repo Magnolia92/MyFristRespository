a1140eaa38507a21c92d08ad909f01423d4c2215
hmz <hmzmandy@163.com>
2016-10-21 15:38:58 +0800

    this is my second commit 2016-10-21 15:38

--- javaplusday01/src/com/ibeifeng/thread/MySynchronized.java
+++ javaplusday01/src/com/ibeifeng/thread/MySynchronized.java
@@ -6,8 +6,8 @@ public class MySynchronized implements Runnable{
 		MySynchronized s=new MySynchronized();
 		Thread t1=new Thread(s);
 		Thread t2=new Thread(s);
-		t1.setName("线程A");
-		t2.setName("线程B");
+		t1.setName("绾跨▼A");
+		t2.setName("绾跨▼B");
 		t1.start();
 		t2.start();
 		
@@ -19,7 +19,7 @@ public class MySynchronized implements Runnable{
 		// TODO Auto-generated method stub
 		while(start <1000){
 			synchronized (this) {
-				//等待唤醒线程
+				
 				notify();
 				if (start++ %2==0){
 					System.out.println(Thread.currentThread().getName()+":1");
@@ -28,7 +28,7 @@ public class MySynchronized implements Runnable{
 				}
 				
 				try {
-					//打印后等待
+				
 					wait();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
